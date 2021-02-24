52746180eec3c45eb6ecc285e45b29ea51a323e9
Robert <s8437293@stud.uni-frankfurt.de>
2021-02-24 16:57:08 +0100

    quick-mergesort

--- leetcode_notebooks/quicksort.cpp
+++ leetcode_notebooks/quicksort.cpp
@@ -1,64 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int A[] = { 5,4,3,2,1 };
+// int A[] = {5, 4, 3, 2, 1};
+// int A[] = [42, 32, 96, 23, 13, 24, 94, 52, 71]
+int A[] = {2, 8, 7, 1, 3, 5, 6, 4};
 
-void printArray() {
-    for (int i : A) {
+void printArray()
+{
+    for (int i : A)
+    {
         cout << i << " ";
     }
     cout << "\n";
 }
 
-void swap(int i, int j) {
+void swap(int i, int j)
+{
     int temp = A[i];
     A[i] = A[j];
     A[j] = temp;
 }
 
-int pivot(int links, int rechts) {
-    return(rechts);
+int pivot(int links, int rechts)
+{
+    int m = int((links + rechts) / 2);
+    cout << "Pivot: " << A[m] << "\n";
+    return (m);
 }
 
-int partition(int p, int links, int rechts) {
+int partition(int p, int links, int rechts)
+{
     int alpha, l, r;
     alpha = A[p];
     l = links - 1;
-    A[l] = INT8_MIN;
     r = rechts;
-    swap(r,p);
-    cout << "p " << p << " l " << l << " r " << r << "\n";
-    do {
-        cout << "Comp l " << l << " " << A[l] << " " << alpha << "\n";
-        while (A[l] <= alpha){
+    swap(r, p);
+    while (l < r)
+    {
+        do
+        {
             l++;
-        }
-        cout << "Comp l " << r << " " << A[r]  << " " << alpha << "\n";
-        while (A[r] >= alpha){
+        } while (A[l] < alpha);
+        cout << "li:" << A[l] << "\n";
+        do
+        {
             r--;
+        } while (A[r] > alpha);
+        cout << "re:" << A[l] << "\n";
+        if (l < r)
+        {
+            swap(l, r);
         }
-        if (l < r){
-            swap(l,r);
-        }
-    } while (l < r);
+    }
     swap(rechts, l);
+    printArray();
     return l;
 }
 
-void quicksort(int links, int rechts) {
+void quicksort(int links, int rechts)
+{
     int p, i;
-    if (links < rechts) {
-        cout << "asd";
+    if (links >= rechts)
+    {
+        cout << "A[links]=" << links << " A[rechts]=" << rechts << "\n";
+    }
+    if (links < rechts)
+    {
+        cout << "A[links]=" << links << " A[rechts]=" << rechts << "\n";
         p = pivot(links, rechts);
         i = partition(p, links, rechts);
-        cout << "Pivot: " << i << " Value: " << A[i] << "\n";
-        quicksort(links, i-1);
-        quicksort(i+1, rechts);
-        printArray();
+        quicksort(links, i - 1);
+        quicksort(i + 1, rechts);
     }
 }
 
-int main() {
+int main()
+{
     printArray();
-    quicksort(0,4);
+    int len = sizeof(A) / sizeof(A[0]);
+    quicksort(0, len - 1);
+    printArray();
 }
