9d2e2a1948ed4fde95d9099e239782e81335b146
0xAnselm@asus.com <0xAnselm@asus.com>
2022-09-05 00:23:56 +0200

    u

--- leetcode_problems/search_Insert_position.cpp
+++ leetcode_problems/search_Insert_position.cpp
@@ -16,12 +16,12 @@ public:
     int recursiveSearch(std::vector<int> &nums, int target, int l, int r)
     {
         int m = (l + r) / 2;
-        if (l >= r) {
+        if (l > r) {
             return m+1;
         }
         if (target < nums[m])
         {
-            return recursiveSearch(nums, target, 0, m - 1);
+            return recursiveSearch(nums, target, l, m - 1);
         }
         else if (target > nums[m])
         {
@@ -38,7 +38,7 @@ public:
 int main()
 {
     std::vector<int> nums = {1, 3, 5, 6};
-    int target = 7;
+    int target = 2;
     Solution sol;
     std::cout << sol.searchInsert(nums, target);
     return 0;
