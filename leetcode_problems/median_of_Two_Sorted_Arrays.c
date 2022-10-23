6d928ce4d4432b4ce3fb358a5c2ccf4f7d8e3892
0xAnselm@asus.com <0xAnselm@asus.com>
2022-10-23 17:24:14 +0200

    u

--- leetcode_problems/median_of_Two_Sorted_Arrays.c
+++ leetcode_problems/median_of_Two_Sorted_Arrays.c
@@ -4,9 +4,8 @@
 
 double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size);
 int *merge(int *nums1, int nums1Size, int *nums2, int nums2Size);
-void printArray(int* nums, int numsSize);
-int* getMedianIdx(int result_len);
-
+void printArray(int *nums, int numsSize);
+int *getMedianIdx(int result_len);
 
 double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Size)
 {
@@ -15,41 +14,61 @@ double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Si
     int p1 = 0;
     int p2 = 0;
     int i = 0;
-    while (true && i < 5) {
-        if (p1 == nums1Size) {
+    while (true && i < 5)
+    {
+        if (p1 == nums1Size)
+        {
             // printf("Break 1\n");
-            const ssize_t size = sizeof(int)*(nums2Size-p2);
+            const ssize_t size = sizeof(int) * (nums2Size - p2);
             memcpy(&result[i], &nums2[p2], size);
             break;
-        } else if (p2 == nums2Size) {
+        }
+        else if (p2 == nums2Size)
+        {
             // printf("Break 2\n");
-            const ssize_t size = sizeof(int)*(nums1Size-p2);
+            const ssize_t size = sizeof(int) * (nums1Size - p2);
             memcpy(&result[i], &nums1[p1], size);
             break;
         }
-        if (nums1[p1] <= nums2[p2]) {
+        if (nums1[p1] <= nums2[p2])
+        {
             result[i] = nums1[p1];
             ++p1;
-        } else {
+        }
+        else
+        {
             result[i] = nums2[p2];
             ++p2;
         }
         ++i;
     }
     printArray(result, result_len);
-    return 2.0;
+    if (result_len % 2)
+    {
+        double ret_val = (result[result_len / 2] + result[(result_len / 2) + 1]);
+        printf("1. Ret vals: %d %d\n", result[(int)result_len / 2], result[(int)(result_len / 2) + 1]);
+        return ret_val;
+    }
+    else
+    {
+        double ret_val = result[(int)(result_len / 2)];
+        printf("2. Ret vals: %d\n", result[(int)result_len / 2]);
+        return ret_val;
+    }
+    return -1;
 }
 
 int main(void)
 {
-    int nums1[] = {1, 3};
+    int nums1[] = {1, 3, 9, 11};
     int nums2[] = {2, 7};
     double a = findMedianSortedArrays(nums1, (int)sizeof(nums1) / sizeof(int), nums2, (int)sizeof(nums2) / sizeof(int));
-    printf("Median: %f", a);
+    printf("Median: %f\nLen Array: %d\n", a, (int)((sizeof(nums1) + sizeof(nums2)) / sizeof(int)));
     return 0;
 }
 
-void printArray(int* nums, int numsSize) {
+void printArray(int *nums, int numsSize)
+{
     printf("Array: ");
     for (int i = 0; i < numsSize; i++)
     {
