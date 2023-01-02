4011b52ca9cf8a506b7b63937139358ba18eb13c
Robert <s8437293@stud.uni-frankfurt.de>
2023-01-02 21:39:27 +0100

    999 x 999

--- leetcode_problems/multiply_Strings.c
+++ leetcode_problems/multiply_Strings.c
@@ -11,41 +11,28 @@ void printA(int* result, int size) {
     printf("\n");
 }
 
+char* convertArray(int* result, int size, int start) {
+    if (result[start] == 0 && size > 1) {
+        return convertArray(result, size - 1, start + 1);
+    }
+
+    char* resultChar = malloc(sizeof(char) * (size + 1));
+    resultChar[size] = '\0';
+
+    for (int i = 0; i < size; ++i, ++start) {
+        resultChar[i] = (char)result[start] + 48;
+    }
+    for (int i = 0; i < size; ++i) {
+        printf("%c", resultChar[i]);
+    }
+    return resultChar;
+}
+
 char* multiply(char* num1, char* num2) {
     int p = (int)(strlen(num1));  // length number 1
     int q = (int)(strlen(num2));  // length number 2
 
-    int* result = calloc((p + q + 1), sizeof(int));
-    result[p + q + 1] = '\0';
-    int resultSize = p + q;
-    int a_i;  // pointer left factor
-    int b_i;  // pointer right factor
-
-    for (b_i = 0; b_i < q; ++b_i) {
-        int carry = 0;
-        for (a_i = p - 1; -1 < a_i; --a_i) {
-            int idx = a_i + b_i + 1;
-            int n = (int)num1[a_i] - 48;
-            int m = (int)num2[b_i] - 48;
-            int val = carry + n * m;
-            result[idx] += val;
-            carry = result[idx] / 10;
-            result[idx] = (char)result[idx] % 10;
-            // printf("n:%d m:%d v:%d\n", n, m, val);
-            // printA(result, resultSize);
-        }
-        result[b_i] += carry;
-    }
-
-    return result;
-}
-
-int* multiplyInt(char* num1, char* num2) {
-    int p = (int)(strlen(num1));  // length number 1
-    int q = (int)(strlen(num2));  // length number 2
-
     int* result = calloc((p + q), sizeof(int));
-    // result[p + q + 1] = '\0';
     int resultSize = p + q;
     int a_i;  // pointer left factor
     int b_i;  // pointer right factor
@@ -60,20 +47,19 @@ int* multiplyInt(char* num1, char* num2) {
             result[idx] += val;
             carry = result[idx] / 10;
             result[idx] = result[idx] % 10;
-            printf("n:%d m:%d v:%d\n", n, m, val);
-            printA(result, resultSize);
+            // printf("n:%d m:%d v:%d\n", n, m, val);
+            // printA(result, resultSize);
         }
         result[b_i] += carry;
     }
-
-    return result;
+    printA(result, resultSize);
+    return convertArray(result, resultSize, 0);
 }
 
 int main() {
-    char* num1 = "195";
-    char* num2 = "12653";
-
-    char* result = multiply(num1, num2);
-    printA(result, (int)(strlen(num1) + strlen(num2)));
+    char* num1 = "999";
+    char* num2 = "999";
+    multiply(num1, num2);
+    // printA(result, (int)(strlen(num1) + strlen(num2)));
     return 0;
 }
