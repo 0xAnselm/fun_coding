3fa85a22a378a5ea125eef12f60965eeb9352b89
Robert <s8437293@stud.uni-frankfurt.de>
2023-01-02 21:11:33 +0100

    u

--- myArch/base-uefi.sh
+++ myArch/base-uefi.sh
@@ -2,18 +2,17 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+# Disable en_US and de_DE
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=de_DE-latin1" >> /etc/vconsole.conf
-echo "FILLMEIN" >> /etc/hostname
+echo "asus" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain FILLMEIN" >> /etc/hosts
-echo root:password | chpasswd
+echo "127.0.1.1 asus.localdomain asus" >> /etc/hosts
+passwd
 
-
-pacman -S --noconfirm efibootmgr dialog iwd reflector base-devel linux-headers avahi nfs-utils inetutils dnsutils openssh acpi acpi_call tlp terminus-font zsh zsh-theme-powerlevel10k-git
+pacman -S networkmanager dialog iwd reflector base-devel linux-headers avahi dnsutils openssh acpi acpi_call tlp terminus-font zsh zsh-theme-powerlevel10k-git
 
 systemctl enable sshd
 systemctl enable avahi-daemon
@@ -22,10 +21,12 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 
 useradd -m r
-echo r:password | chpasswd
+passwd r
 
 echo "r ALL=(ALL) ALL" >> /etc/sudoers.d/r
 echo "Think about oh-my-zsh-git and chsh -s /usr/bin/zsh"
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 # chmod +x base-uefi.sh
 
