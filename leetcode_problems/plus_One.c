e88678db4f082e2b93c8084063fb1ed07be27760
0xAnselm@asus.com <a@b.c>
2023-01-02 20:51:04 +0100

    u

--- leetcode_problems/plus_One.c
+++ leetcode_problems/plus_One.c
@@ -2,17 +2,63 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <string.h>
+
+int* plusOne_attempt1(int* digits, int digitsSize, int* returnSize);
+
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int carry = 1;
+    int currentPos = digitsSize - 1;
+    *returnSize = digitsSize;
+    while ((carry != 0) && (currentPos >= 0)) {
+        // check if digit at currenPos + 1 overflows
+        if ((digits[currentPos] + 1) > 9) {
+            digits[currentPos] = 0;
+            carry = 1;
+        } else {
+            digits[currentPos]++;
+            carry = 0;
+        }
+        currentPos--;
+    }
+    int* result;
+    if ((carry == 1) && (currentPos == -1)) {
+        *returnSize = digitsSize + 1;
+        result = malloc(*returnSize * sizeof(int));
+        result[0] = digits[0] + carry;
+        memcpy(&result[1], digits, sizeof(int) * digitsSize);
+        return result;
+    } else if (carry == 0) {
+        result = malloc(digitsSize * sizeof(int));
+        memcpy(result, digits, sizeof(int) * digitsSize);
+        return result;
+    }
+    return NULL;
+}
+
+int main(void) {
+    int digitsSize = 4;
+    int* digits = malloc(sizeof(int) * digitsSize);
+    digits[0] = 4;
+    digits[1] = 3;
+    digits[2] = 2;
+    digits[3] = 1;
+    int returnSize = 4;
+    int* result = plusOne(digits, digitsSize, &returnSize);
+    for (int i = 0; i < returnSize; ++i) {
+        printf("%d\n", result[i]);
+    }
+    free(result);
+    free(digits);
+}
+
+int* plusOne_attempt1(int* digits, int digitsSize, int* returnSize) {
     uintmax_t val = 0;
     for (int i = 0; i < digitsSize; ++i) {
-        val += digits[i] * pow(10, digitsSize - 1 - i);
+        val += digits[i] * (uintmax_t)pow(10, digitsSize - 1 - i);
         printf("%ld\n", val);
     }
     val++;
-    val = 6145390195186705544;
     *returnSize = (int)log10(val) + 1;
     printf("val: %ld, ret size: %d\n", val, *returnSize);
     int* ret = malloc(sizeof(int) * (*returnSize));
@@ -21,7 +67,7 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
     for (int i = 0; i < *returnSize; ++i) {
         uintmax_t power = pow(10, pos - i);
         uintmax_t r = val / power;
-        printf("v: %ld, p: %ld, q: %lld", val, power, r);
+        printf("v: %ld, p: %ld, q: %ld", val, power, r);
         ret[i] = r;
         printf("%d\n", ret[i]);
         val -= (ret[i] * power);
@@ -30,11 +76,4 @@ int* plusOne(int* digits, int digitsSize, int* returnSize) {
         printf("%4d", ret[i]);
     }
     return ret;
-}
-
-int main(void) {
-    int digits[] = {6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3};
-    int digitsSize = 19;
-    int returnSize = 19;
-    plusOne(digits, digitsSize, &returnSize);
 }
